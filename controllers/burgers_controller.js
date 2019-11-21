6088bd356d0a69d35df19cbb20be01129f773b8e
Raymond _ <raymondt1234@gmail.com>
2019-11-21 14:17:30 -0700

    added controller file burger_controller.js

---
+++ controllers/burgers_controller.js
@@ -0,0 +1,44 @@
+const express = require("express");
+
+const router = express.Router();
+
+const burger = require("../models/cat.js");
+
+router.get("/", function(req, res) {
+  burger.all(function(data) {
+    let hbsObject = {
+      burgers: data
+    };
+    console.log(hbsObject);
+    res.render("index", hbsObject);
+  });
+});
+
+router.post("/api/burgers", function(req, res) {
+  burger.create([
+    "burger_name", "devoured"
+  ], [
+    req.body.burger_name, req.body.devoured
+  ], function(result) {
+    res.json({ id: result.insertId });
+  });
+});
+
+router.put("/api/burgers/:id", function(req, res) {
+  var condition = "id = " + req.params.id;
+
+  console.log("condition", condition);
+
+  burger.update({
+    sleepy: req.body.devoured
+  }, condition, function(result) {
+    if (result.changedRows == 0) {
+      return res.status(404).end();
+    } else {
+      res.status(200).end();
+    }
+  });
+});
+
+
+module.exports = router;
