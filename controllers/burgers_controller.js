dab7cfa2c3250bb95aa0e0094cc3869702f87be7
Raymond _ <raymondt1234@gmail.com>
2019-11-21 14:18:17 -0700

    added controller file burger_controller.js

--- controllers/burgers_controller.js
+++ controllers/burgers_controller.js
@@ -2,7 +2,7 @@ const express = require("express");
 
 const router = express.Router();
 
-const burger = require("../models/cat.js");
+const burger = require("../models/burger.js");
 
 router.get("/", function(req, res) {
   burger.all(function(data) {
